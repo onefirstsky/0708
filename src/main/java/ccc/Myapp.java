144abf66528353d337beec32ad19b36685b453eb
Administrator <1005643804@qq.com>
2019-12-09 11:22:06 +0800

    xxxxx

---
+++ src/main/java/ccc/Myapp.java
@@ -0,0 +1,10 @@
+package ccc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Myapp {
+    public static void main(String[] args) {
+        SpringApplication.run(Myapp.class,args);
+    }
+}
